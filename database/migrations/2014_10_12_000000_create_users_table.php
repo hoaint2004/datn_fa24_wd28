46635a3c0af6bf0d94875840d9f7cb5e637a43b0
hoaintp <hoaintph36134@fpt.edu.vn>
2024-10-09 17:29:00 +0700

    database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname', 255);
+            $table->string('username')->unique(); 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
+            $table->enum('role', ['admin', 'user'])->default('user'); 
             $table->rememberToken();
             $table->timestamps();
         });
