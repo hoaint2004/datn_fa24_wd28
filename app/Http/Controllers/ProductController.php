8c5399737281cb4628da58acb9bb072a86989821
Phamanh589785 <phamanh589785@gmail.com>
2024-11-04 22:17:30 +0700

    register

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
-
+    public function home(){
+        return view('user.index');
     }
 
     public function create(){
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'fullname', 'username', 'email', 'phone', 'password',
     ];
 
     /**
