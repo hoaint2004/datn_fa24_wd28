bdadbdae5ea16ba362aed1716ded00eb8337807c
datdata01 <datdata06@gmail.com>
2024-11-04 21:25:03 +0700

    quan ly account

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class CategoryController extends Controller
 {
     public function product(){
        return 0;
