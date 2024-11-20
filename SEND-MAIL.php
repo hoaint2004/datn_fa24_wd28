29600f0f2810d0b858bcd727f7dfc289849b3c9b
NguyenDat <datdata01@gmail.com>
2024-11-20 17:36:41 +0700

    ThongTin FILE.ENV

---
+++ SEND-MAIL.php
@@ -0,0 +1,8 @@
+MAIL_MAILER=smtp
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USERNAME=datntph40630@fpt.edu.vn
+MAIL_PASSWORD="a s p w j p f f i t z k m m j c"
+MAIL_ENCRYPTION=tls
+MAIL_FROM_ADDRESS="ongtrum@gmail.com"
+MAIL_FROM_NAME="ôngtrùmgiàydép@yêurâuxanh.com"
