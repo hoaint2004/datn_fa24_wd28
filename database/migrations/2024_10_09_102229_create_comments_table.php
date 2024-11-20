53cf42859feca55c6ed8468ffcb9cb6b96f319e0
Phamanh589785 <phamanh589785@gmail.com>
2024-11-20 12:41:18 +0700

    update migrate comments

--- database/migrations/2024_10_09_102229_create_comments_table.php
+++ database/migrations/2024_10_09_102229_create_comments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('user_id');
             $table->text('content');
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
