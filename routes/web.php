06e13e12734e66130752e79d659b1e5a04e53852
Hieu02072004 <nguyenminhhieust10@gmail.com>
2024-11-22 00:53:09 +0700

    suahome

--- routes/web.php
+++ routes/web.php
@@ -111,7 +111,17 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::post('/delete/{id}', 'delete')
                 ->name('delete');
         });
-        Route::resource('banners', BannerController::class);
+        Route::controller(BannerController::class)
+        ->name('banners.')
+        ->prefix('banners')
+        ->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/store', 'store')->name('store');
+            Route::get('/edit/{id}', 'edit')->name('edit');
+            Route::put('/update/{id}', 'update')->name('update');
+            Route::delete('/delete/{id}', 'delete')->name('delete');
+        });
 
     Route::resource('orders', AdminOrderController::class);
 });
