8dbf3eef1cd1852505c5cf547070594104ca49c7
nkquan <khacquan712004@gmail.com>
2024-11-24 19:45:06 +0700

    update order and order admin

--- database/migrations/2024_10_09_101030_create_orderstatus_table.php
+++ database/migrations/2024_10_09_101030_create_orderstatus_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('_orderstatus');
+        Schema::dropIfExists('orderstatus');
     }
 };
